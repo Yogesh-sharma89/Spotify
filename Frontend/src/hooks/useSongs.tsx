2b71f601f8e60e5b6183d8f5d5f649f212bac872
Yogesh <nitya.upmanyu@gmail.com>
2026-05-13 13:52:51 +0530

    fix some issues

--- Frontend/src/hooks/useSongs.tsx
+++ Frontend/src/hooks/useSongs.tsx
@@ -2,37 +2,35 @@ import type { Song } from "@/interfaces/song"
 import { useQuery } from "@tanstack/react-query"
 import axios from "axios"
 
-interface songsResponse{
-    success:boolean,
-    songs:Song[],
-    message:string
+const production_url = import.meta.env.PRODUCTION_URL;
+
+interface songsResponse {
+  success: boolean
+  songs: Song[]
+  message: string
 }
 
+export const useGetSpecificSongs = (size: number, url: string) => {
+  const { data, isLoading } = useQuery({
+    queryKey: ["songs", size, url],
+    queryFn: async () => {
+      const res = await axios.get(
+        `${production_url}/api/songs/${url}`,
+        {
+          withCredentials: true,
+          params: {
+            size,
+          },
+        }
+      )
 
+      return res.data as songsResponse
+    },
+    retry: 3,
+    refetchOnWindowFocus: false,
+    refetchOnReconnect: false,
+    staleTime: 1000 * 60 * 5,
+  })
 
-export const useGetSpecificSongs = (size:number,url:string)=>{
-
-    const {data,isLoading} = useQuery({
-
-        queryKey:['songs',size,url],
-        queryFn:async()=>{
-
-            const res = await axios.get(`https://spotify-production-4f14.up.railway.app/api/songs/${url}`,{
-                withCredentials:true,
-                params:{
-                    size
-                }
-            })
-
-            return res.data as songsResponse;
-
-
-        },
-        retry:3,
-        refetchOnWindowFocus:false,
-        refetchOnReconnect:false,
-        staleTime:1000*60*5,
-    })
-
-    return {songs:data?.songs,isLoading};
+  return { songs: data?.songs, isLoading }
 }
